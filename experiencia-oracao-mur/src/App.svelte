fdcaf3b34d8693adc294b24154ce6350f1cb7e19
Marcos Rios <marcoslucsrios@gmail.com>
2022-03-02 15:47:00 -0300

    feature: creating svelte project and adding configuration for importation of assets and sass in rollup config

---
+++ experiencia-oracao-mur/src/App.svelte
@@ -0,0 +1,22 @@
+<script>
+	import Header from './components/Header.svelte'
+	export let name;
+</script>
+
+<main>
+	<Header/>
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
+
+<style lang="scss">
+	$color: yellow;
+	h1 {
+		color: $color
+	}
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ experiencia-oracao-mur/src/components/Header.svelte
@@ -0,0 +1,111 @@
+<header>
+    <img class="logo" src={logoImg} alt="Logo MUR Lavras"/>
+    <nav>
+        <div class="content">
+            <button class="content__menu-button">
+                {#if !showMobileMenu}
+                    <img on:click={handleMobileMenuClick} src={menuImg} alt=""/>
+                {:else}
+                    <img on:click={handleMobileMenuClick} src={closeImg} alt=""/>
+                {/if}
+            </button>
+            <ul class={`content__navbar-list${showMobileMenu ? ' content__navbar-list--mobile' : ''}`}>
+                {#each navItems as item}
+                <li>
+                    <a href={item.href}>{item.label}</a>
+                </li>
+                {/each}
+            </ul>
+        </div>
+    </nav>
+</header>
+
+<script>
+    import logoImg from "../assets/logo.jpeg";
+    import menuImg from "../assets/menu.svg"
+    import closeImg from "../assets/close.png"
+    import { onMount } from "svelte";
+
+    const navItems = [
+        { label: "logo", href: "#" },
+        { label: "Item 1", href: "#" },
+        { label: "Item 2", href: "#" },
+        { label: "Item 3", href: "#" },
+        { label: "Item 4", href: "#" },
+        { label: "Item 5", href: "#" },
+        { label: "Item 6", href: "#" },
+        { label: "Item 7", href: "#" }
+    ];
+
+    onMount(() => {
+        const windowMediaEvetnListener = window.matchMedia("(max-width: 767px)");
+        windowMediaEvetnListener.addEventListener(mediaEventQueryHandler);
+    });
+
+    let showMobileMenu = false;
+
+    const handleMobileMenuClick = () => (showMobileMenu = !showMobileMenu)
+
+    const mediaEventQueryHandler = event => {
+        if (!event.matches) {
+            showMobileMenu = false;
+        }
+    };
+
+</script>
+
+<style type="scss">
+    header {
+        display: flex;
+        align-items: center;
+        flex-direction: row;
+        justify-content: space-between;
+        background-color: transparent;
+        height: 60px;
+        padding: 0 20px;
+
+        .logo {
+            width: 55px;
+            height: auto;
+        }
+
+        nav {
+            .content {
+                display: flex;
+                align-items: center;
+
+                &__menu-button {
+                    border: none;
+                    background: none;
+                    
+                    @media only screen and (min-width: 720px) {
+                        display: none;
+                    }
+
+                    img {
+                        width: 24px;
+                        height: auto;
+                    }
+                } 
+
+                &__navbar-list {
+                    display: flex;
+                    list-style-type: none;
+                    flex-direction: row;
+                    
+                    li + li {
+                        margin-left: 10px;
+                    }
+
+                    
+                    @media only screen and (max-width: 720px) {
+                        flex-direction: column;
+                    }
+                }
+                
+            }
+        }
+
+    }
+</style>
+    
